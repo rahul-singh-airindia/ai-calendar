dbe26f42f8d885708556ed496aefdc48aafeccd5
sonu-airindia <sonu4@airindia.com>
2024-02-27 14:11:21 +0530

    add: month

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'aiCalendar';
+  currentView: 'month' | 'week' | 'day' = 'month';
+
+  onViewChange(newView: 'month' | 'week' | 'day') {
+    this.currentView = newView;
+  }
+
+  onNavigation(action: 'prev' | 'next') {
+    // Handle navigation logic here
+    console.log(`${action} clicked`);
+  }
+
 }
--- src/components/month/month.component.ts
+++ src/components/month/month.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+// import { CalendarService } from '../header/calender.service';
 
 @Component({
   selector: 'app-month',
   templateUrl: './month.component.html',
-  styleUrls: ['./month.component.css']
+  styleUrls: ['./month.component.scss']
 })
-export class MonthComponent implements OnInit {
+export class MonthComponent  implements OnInit {
+  currentDate: Date = new Date();
+  monthName: string = '';
+  daysInMonth: number = 0;
+  firstDayOfWeek: number = 0;
+  emptyDays: any[] = [];
+  days: number[] = [];
+ 
+  
 
-  constructor() { }
+  // constructor(public calenderService: CalendarService){}
 
   ngOnInit(): void {
+    // this.calenderService.getDays().subscribe((data) =>{this.updateMonth(data);} );
+  }
+  private updateMonth(date: Date): void {
+    this.currentDate = date;
+    this.monthName = this.getMonthName(date);
+    this.daysInMonth = this.getDaysInMonth(date);
+    this.firstDayOfWeek = this.getFirstDayOfWeek(date);
+    this.generateDays();
+  }
+  
+
+  private getMonthName(date: Date): string {
+    return date.toLocaleString('en-US', { month: 'long' });
+  }
+
+  private getDaysInMonth(date: Date): number {
+    const lastDayOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+    return lastDayOfMonth.getDate();
+  }
+
+  private getFirstDayOfWeek(date: Date): number {
+    const firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
+    return firstDayOfMonth.getDay();
+    
+  }
+
+  private generateDays(): void {
+    this.emptyDays = Array(this.firstDayOfWeek).fill(null);
+    this.days = Array.from({ length: this.daysInMonth }, (_, i) => i + 1);
   }
 
 }
